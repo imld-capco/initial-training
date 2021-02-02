1a8636df6242b65eb944f7c1ce8be6006eb12895
Ivo Mladenov <ivo.mladenov@capco.com>
2021-02-02 15:31:43 +0000

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest(classes = DemoApplication.class)
+class DemoApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
